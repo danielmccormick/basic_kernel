9b6d5bab82876da8007598d67f9cbde83273e562
Daniel McCormick <danielmccormick99@gmail.com>
2019-06-13 09:45:45 -0400

    kernel?

---
+++ kernel.h
@@ -0,0 +1,9 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+/* Type Definitions */
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+/* What do you even do ?? */
