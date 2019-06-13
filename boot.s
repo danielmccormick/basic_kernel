279f591ceca074dd56e68bf1238deca633c2546b
Daniel McCormick <danielmccormick99@gmail.com>
2019-06-12 20:38:24 -0400

    bootloader attempt 1

---
+++ boot.s
@@ -0,0 +1,48 @@
+# set magic number to 0x1BADB002
+.set MAGIC, 0x1BADB002
+
+# NO flags - no additional fields needed for now
+.set FLAGS, 0
+
+# checksum must be magic+flags
+.set CHECKSUM, -(MAGIC+FLAGS)
+
+# Multiboot section
+.section .multiboot
+
+# must be u32, and long obstensibly is 4 bytes
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+# stackBottom first because since sp goes high to low
+stackBottom:
+
+# max size 4096 bytes
+.skip 4096
+
+stackTop:
+
+#text section
+.section .text
+
+# for future linking 
+.global _start
+
+# Declares start function
+.type _start @function
+
+# Startup functoin
+_start:
+	#move SP
+	mov $stackTop, %esp
+	# deadLoop until I write more lol
+	call deadLoop
+# Loop forever
+deadLoop:
+	# Wait until interrupt
+	hlt
+	# Clear interrupt
+	cli
+	# Go back to deadLoop label
+	jmp deadLoop
