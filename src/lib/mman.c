e4d1332cb61e37f47743a01f3af6b5cc5f898f62
Daniel McCormick <danielmccormick99@gmail.com>
2019-11-01 14:06:02 -0400

    patched lib functionality

--- src/lib/mman.c
+++ src/lib/mman.c
@@ -9,31 +9,31 @@ void mman_init() {
 // no recombinations for now
 void *malloc(size_t size) {
 	int pad = 0;
-	size_t min_chunk_size = 2*sizeof(size_t)+2*sizeof(uint8_t)+sizeof(chunk_t*)+size;
-       	if (pad = (min_chunk_size%4)) {
+	size_t min_chunk_size = 2*sizeof(size_t)+2*sizeof(uint8_t)+sizeof(_chunk_t*)+size;
+       	if ( (pad = (min_chunk_size%4)) ) {
 		pad = 4-pad;
 		size += pad;
 		min_chunk_size += pad;
 	
 	}
-	if (!mman_head) {
+	if (!mman_head_) {
 		mman_init();
 	}
-	_chunk_t trav* = mman_head;
+	_chunk_t *trav = mman_head_;
 	while (trav->flags != MCHUNK_INITIALIZED &&
-		!(trav->flags == MCHUNK_FREED && trav->size >= min_chunk_size) {
+		!(trav->flags == MCHUNK_FREED && trav->size >= min_chunk_size)) {
 		trav = trav->next;	
 	}
-	if (trav->flags = MCHUNK_FREED) {
+	if (trav->flags == MCHUNK_FREED) {
 		trav->flags &= (~MCHUNK_FREED);
 		trav->flags |= (MCHUNK_ALLOCATED);
 		return &(trav->data);
 	}
-	else if (&(trav->data)+(min_chunk_size-1) >= _HEAP_END) { return (void*)NULL; }
+	else if ((void*)&(trav->data)+(min_chunk_size-1) >= _HEAP_END_) { return (void*)0; }
 	trav->size = size;
 	trav->flags &= (~MCHUNK_INITIALIZED);
 	trav->flags |= MCHUNK_ALLOCATED;
-	trav->next = (&(trav->data)+min(chunk_size-1));
+	trav->next = &(_chunk_t *)((trav->data)+(char*)(min_chunk_size-1));
 	return &(trav->data);
 }
 
